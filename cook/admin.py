7063878e12543f8b07309159a4fd5ffd9768795a
modle <odle.matthew@gmail.com>
2015-07-01 22:56:13 -0500

    Redesigned models Renamed app Added template filters

--- cook/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Recipe, Ingredient, RecipeIngredient
-
-
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
-admin.site.register(RecipeIngredient)
--- triggerfoods/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from .models import TriggerFood
-
-# Create your views here.
-def index(request):
-    triggerfood_query = TriggerFood.objects.all()  #[:5]
-    context = {'triggerfood_query': triggerfood_query}
-    return render(request, 'triggerfoods/index.html', context)
